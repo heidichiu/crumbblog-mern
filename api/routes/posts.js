12211af0eca7c5143300243ea33b3b5d79b78021
heidichiu <heidi@xqtem.com>
2021-12-31 06:46:13 +0800

    Add the CRUD operations for posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -51,14 +51,51 @@ router.put("/:id", authenticateToken, async (req, res) => {
 });
 
 // delete post
-router.delete("/:id", authenticateToken, async (req, res) => {});
+router.delete("/:id", authenticateToken, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.user.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted.");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can only delete your post.");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // get post
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const post = await Post.findById(req.params.id);
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get all posts
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const category = req.query.category;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (category) {
+      posts = await Post.find({ categories: { $in: [category] } });
+    } else if (username && category) {
+      posts = await Post.find({ categories: { $in: [category] }, username });
+    } else {
+      posts = Post.find();
+    }
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
